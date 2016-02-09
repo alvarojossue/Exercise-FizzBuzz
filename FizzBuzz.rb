f195252b6432b4df065fe8587799742e3421384a
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-02-08 20:04:09 -0500

    iteration 3

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,13 +1,23 @@
-num = 1
-while num <= 100
-    if num % 3 == 0 && num % 5 == 0
+for num in 1..100
+ if num % 3 == 0 && num % 5 == 0 && num > 9 && num < 20
+     puts "FizzBuzzBang"
+    elsif num % 3 == 0 && num > 9 && num < 20
+        puts "FizzBang"
+    elsif num % 5 == 0 && num > 9 && num < 20
+        puts "BuzzBang"
+    elsif num == 100
+        puts "BuzzBang"
+    elsif num % 3 == 0 && num % 5 == 0
         puts "FizzBuzz"
     elsif num % 5 == 0
         puts "Buzz"
     elsif num % 3 == 0
         puts "Fizz"
+    elsif num > 9 && num < 20
+        puts "Bang"
+    elsif num == 1
+        puts "Bang"
     else
         puts "#{num}"
     end
-num = num + 1
 end
